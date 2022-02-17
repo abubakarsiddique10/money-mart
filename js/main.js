0e10aa6e4a578fd79d253a283da440540e0b4609
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-02-17 22:54:05 +0600

    total cost and total balance complite

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
 
-function inputValue(inputId) {
+function inputValue(inputId, i) {
     let inputTex = document.getElementById('total-' + inputId);
     let inputNumber = parseFloat(inputTex.value);
-    inputTex.value = '';
-    return inputNumber;
+    if (isNaN(inputNumber)) {
+        document.getElementsByClassName('error')[i].style.display = 'block';
+        inputTex.value = '';
+    }
+    else {
+        inputTex.value = '';
+        return inputNumber;
+    }
 }
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    let totalIncome = inputValue('income');
-    let foodCost = inputValue('food-cost');
-    let rentCost = inputValue('rent-cost');
-    let clothCost = inputValue('cloth-cost');
+    let totalIncome = inputValue('income', 0);
+    let foodCost = inputValue('food-cost', 1);
+    let rentCost = inputValue('rent-cost', 2);
+    let clothCost = inputValue('cloth-cost', 3);
     // calculating total income and total cost
     let totalCost = foodCost + rentCost + clothCost;
-    let Balance = totalIncome - totalCost;
-    document.getElementById('total-expenses').innerText = totalCost;
-    document.getElementById('balance').innerText = Balance;
+    if (totalCost > totalIncome) {
+        console.log('vole number')
+    }
+    else {
+        if (typeof totalIncome == 'number' && typeof totalCost == 'number') {
+            let Balance = totalIncome - totalCost;
+            document.getElementById('total-expenses').innerText = totalCost;
+            document.getElementById('balance').innerText = Balance;
+        }
+    }
 })
 document.getElementById('save-btn').addEventListener('click', function () {
     // select total expenses, balance and calculating
@@ -25,10 +38,15 @@ document.getElementById('save-btn').addEventListener('click', function () {
     let totalIncome = totalExpenses + balance;
     // get parcentage input value
     let parcentage = inputValue('parcentage-input')
+    if (isNaN(parcentage)) {
+        console.log('please enter numer')
+    }
     // select saving amount, remaining balance and calculating
-    let saving = document.getElementById('savign-amount');
-    let savingAmount = totalIncome * parcentage / 100;
-    saving.innerText = savingAmount
-    let reaminingBalance = document.getElementById('remaining-balance');
-    reaminingBalance.innerText = balance - savingAmount;
+    else {
+        let saving = document.getElementById('savign-amount');
+        let savingAmount = totalIncome * parcentage / 100;
+        saving.innerText = savingAmount
+        let reaminingBalance = document.getElementById('remaining-balance');
+        reaminingBalance.innerText = balance - savingAmount;
+    }
 })
